13afaf2881d3a2105d1557341f0d2fb816269504
anmendeza <andrea@firstfactory.com>
2021-10-17 11:24:03 -0600

    ejercicio 2

--- homework-2/cypress/integration/exercise2_spec.js
+++ homework-2/cypress/integration/exercise2_spec.js
@@ -4,15 +4,20 @@ describe('Exercise 2', () => {
         cy.visit('/challenges/actions')
     });
     context('Part 1: Focus', () =>{
-        it('Should type email inside the email input', () => {
-            cy.get('#password1').type('1q2w3e4r').click();
-            cy.get('label').contains('Password').should('have.css','color','orange');
+        it('Should turn label & input border into orange color', () => {
+            cy.get('#password1').type('1q2w3e4r');
+            cy.get('#password1').prev().should('have.attr','style', 'color: orange;')
+            cy.get('#password1').prev().should('have.css','border', '0px none rgb(255, 165, 0)')
         });
-    })
+    });
+
     context('Part 2: Blur', () => {
-        //it('Should send ctrl A to select and delete input', () => {
-        //    cy.get('#password1').type('1q2w3e4r').click();
-        //    cy.get('#password1').should('have.css', 'border', '5px solid orange')
-        //});
+        it('Should turn label and input border into red color', () => {
+            cy.get('#fullName1').click();
+            cy.get('#fullName1').prev().click();
+            cy.get('#fullName1').prev().should('have.attr','style', 'color: red;')
+            cy.get('#fullName1').prev().should('have.css','border', '0px none rgb(255, 0, 0)')
+
+        });
     })
 });
